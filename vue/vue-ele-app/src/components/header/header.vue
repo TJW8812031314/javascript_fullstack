dc1441a82287c68643d1e0af48fe6aefe63e65e4
1362368453@qq.com <1362368453@qq.com>
2019-11-01 18:22:41 +0800

    vue- 外卖app

---
+++ vue/vue-ele-app/src/components/header/header.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="header">
+    <div class="content-wrapper">
+      <div class="avatar">
+        <img src="./brand@2x.png" alt="">
+      </div>
+      <div class="content">
+        <div class="title">
+          <div class="brand"></div>
+          <span class="name">粥品香坊</span>
+        </div>
+        <div class="descriptipon">蜂鸟专送38分钟送达</div>
+        <div class="support">
+          <span class="icon special"></span>
+          <span class="text">在线支付满28减5</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  created () {
+    this.$http.get('http://ustbhuangyi.com/sell/api/seller')
+      .then((res) => {
+        console.log(res)
+      })
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '../../common/stylus/mixin.styl'
+.header
+  position relative
+  color #ffffff
+  background rgba(7, 17, 27, 0.5)
+  .content-wrapper
+    position relative
+    padding 24px 12px 18px 24px
+    font-size 0
+    .avatar
+      display inline-block
+      // 拦腰对齐
+      vertical-align top
+      img
+        border-radius 2px
+    .content
+      margin-left 16px
+      font-size 14px
+      display inline-block
+      .title
+        margin 2px 0 8px 0
+        .brand
+          width 30px
+          height 18px
+          display inline-block
+          bg-image('brand')
+          background-size 30px 18px
+          background-repeat no-repeat
+          vertical-align middle
+        .name
+          margin-left 6px
+          font-size 16px
+          line-height 18px
+          font-weight bold
+          vertical-align middle
+      .descriptipon
+        margin-bottom 10px
+        line-height 12px
+        font-size 12px
+      .support
+        .icon
+          display inline-block
+          vertical-align top
+          width 12px
+          height 12px
+          margin-right 4px
+          background-size 12px 12px
+          background-repeat no-repeat
+          &.decrease
+            bg-image('decrease_1')
+          &.discount
+            bg-image('discount_1')
+          &.guarantee
+            bg-image('guarantee_1')
+          &.invoice
+            bg-image('invoice_1')
+          &.special
+            bg-image('special_1')
+        text
+          line-height 12px
+          font-size 14px
+</style>
