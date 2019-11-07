532bb61a5d1623845f09cc0ad5b46301f8689bce
1362368453@qq.com <1362368453@qq.com>
2019-11-07 21:46:32 +0800

    ref使用 页面联动

--- vue/vue-ele-app/src/components/goods/goods.vue
+++ vue/vue-ele-app/src/components/goods/goods.vue
@@ -1,38 +1,78 @@
 <template>
   <div>
     <div class="goods">
-      <div class="menu-wrapper" ref="menuwrapper">
+      <div class="menu-wrapper" ref="menuWrapper">
         <ul>
           <li
-          v-for="(item , index) in goods"
+          v-for="(item, index) in goods"
           :key="index"
           class="menu-item"
           @click="selectMenu(index)"
           :class="{'current': currentIndex === index}"
           >
-            <span class="text borde-1px">
+            <span class="text border-1px">
               <span v-if="item.type > 0" class="icon" :class="classMap[item.type]"></span>
               {{item.name}}
             </span>
           </li>
         </ul>
       </div>
-      <div class="foods-wrapper">
+      <div class="foods-wrapper" ref="foodsWrapper">
+        <ul>
+          <li
+          class="food-list"
+          v-for="(item, index) in goods"
+          :key="index"
+          ref="foodList"
+          >
+            <h1 class="title">{{item.name}}</h1>
+            <ul>
+              <li
+              class="food-item border-1px"
+              v-for="(food, index) in item.foods"
+              :key="index"
+              >
+                <div class="icon">
+                  <img :src="food.icon" alt="">
+                </div>
+                <div class="content">
+                  <h2 class="name">{{food.name}}</h2>
+                  <p class="desc">{{food.description}}</p>
+                  <div class="extra">
+                    <span class="count">月售{{food.sellCount}}份</span>
+                    <span class="">好评率{{food.rating}}%</span>
+                  </div>
+                  <div class="price">
+                    <span class="now">￥{{food.price}}</span>
+                    <span class="old" v-if="food.oldPrice">￥{{food.oldPrice}}</span>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </li>
+        </ul>
       </div>
     </div>
+    <!-- 购物车 -->
+    <shopcart></shopcart>
   </div>
 </template>
 
 <script>
 import Bscroll from 'better-scroll'
+import shopcart from '@/components/shopcart/shopcart.vue'
 export default {
   name: 'Goods',
   data () {
     return {
-      goods: [],
+      goods: [1, 2, 3, 4, 5, 6, 7, 8, 9],
       classMap: ['decrease', 'discount', 'special', 'invoice', 'guarantee'],
-      currentIndex: 0
+      listHeight: [],
+      scrollY: 0
     }
+  },
+  components: {
+    shopcart
   },
   created () {
     this.$http.get('http://localhost:8080/static/goods.json')
@@ -43,20 +83,63 @@ export default {
           // html 渲染之后调用—initScroll方法
           this.$nextTick(() => {
             this._initScroll()
+            this._calculateHeight()
           })
         }
       })
   },
+  computed: {
+    currentIndex () {
+      for (let i = 0; i < this.listHeight.length; i++) {
+        let height1 = this.listHeight[i]
+        let height2 = this.listHeight[i + 1]
+        // 如果height2 不存在 或 滚动不超过第一菜单与
+        if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+          return i
+        }
+      }
+      return 0
+    }
+  },
   methods: {
     _initScroll () {
       // 获取动结构
-      this.menuScrall = new Bscroll(this.$refs.menuwrapper, {
+      this.menuScrall = new Bscroll(this.$refs.menuWrapper, {
         click: true
       })
+      // refs不是获取动结构的方法，只是拥有相同的能力
+      this.foodsScrall = new Bscroll(this.$refs.foodsWrapper, {
+        click: true,
+        probeType: 3
+      })
+      // 监听滚动事件
+      this.foodsScrall.on('scroll', pos => {
+        // console.log(pos)
+        // 取整 取绝对值
+        this.scrollY = Math.abs(Math.round(pos.y))
+      })
     },
+    // 拿到下标
     selectMenu (idx) {
       console.log(idx)
-      this.currentIndex = idx
+      // this.currentIndex = idx
+      let foodList = this.$refs.foodList
+      // console.log(foodList)
+      let el = foodList[idx]
+      // 滚动到指定结构
+      this.foodsScrall.scrollToElement(el, 300)
+    },
+    _calculateHeight () {
+      let foodList = this.$refs.foodList
+      let height = 0
+      this.listHeight.push(height)
+      for (let i = 0; i < foodList.length; i++) {
+        let item = foodList[i]
+        height += item.clientHeight
+        this.listHeight.push(height)
+      }
+      console.log(this.listHeight)
+      // clientHeigth 获取当前容器高度
     }
   }
 }
@@ -114,5 +197,61 @@ export default {
         vertical-align middle
         border-1px(rgba(7, 17, 27, 0.1))
         font-size 12px
-
+.foods-wrapper
+    // overflow scroll超出滚动
+    flex 1
+    .title
+      padding-left 14px
+      height 26px
+      line-height 26px
+      border-left 2px solid #d9dde1
+      font-size 12px
+      color rgb(147, 153, 159)
+      background #f3f5f7
+    .food-item
+      display flex
+      margin 18px
+      padding-bottom 18px
+      border-1px(rgba(7, 17, 27, 0.1))
+      &:last-child
+        border-none()
+        margin-bottom 0
+      .icon
+        flex 0 0 57px
+        margin-right 10px
+        img
+          width 100%
+      .content
+        flex 1
+        .name
+          margin 2px 0 8px 0
+          height 14px
+          line-height 14px
+          font-size 14px
+          color rgb(7, 17, 27)
+        .desc, .extra
+          line-height 10px
+          font-size 10px
+          color rgb(147, 153, 159)
+        .desc
+          line-height 12px
+          margin-bottom 8px
+        .extra
+          .count
+            margin-right 12px
+        .price
+          font-weight 700
+          line-height 24px
+          .now
+            margin-right 8px
+            font-size 14px
+            color rgb(240, 20, 20)
+          .old
+            text-decoration line-through
+            font-size 10px
+            color rgb(147, 153, 159)
+        .cartcontrol-wrapper
+          position absolute
+          right 0
+          bottom 12px
 </style>
