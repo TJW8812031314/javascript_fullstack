48c8de92f07aa3eae8e113ff516a22a1e726d0a8
1362368453@qq.com <1362368453@qq.com>
2019-11-19 14:26:23 +0800

    分糖果

--- vue/vue-ele-app/src/components/ratings/ratings.vue
+++ vue/vue-ele-app/src/components/ratings/ratings.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="ratings">
-    <div class="rating-content">
+    <div class="ratings-content" ref="ratingsContent">
       <div class="overview">
         <div class="overiview-left">
           <h1 class="scroe">{{seller.score}}</h1>
@@ -11,20 +11,28 @@
           <div class="score-wrapper">
             <span class="title">服务态度</span>
             <div class="star star-36">
-              <span class="star-item" v-for="index of 5" :key="index" :class="{'on':seller.serviceScore > 0}"></span>
+              <span class="star-item"
+              v-for="(item, index) in className"
+              :key="index"
+              :class="className"
+              ></span>
             </div>
             <span class="score">{{seller.serviceScore}}</span>
           </div>
           <div class="score-wrapper">
             <span class="title">商品评分</span>
             <div class="star star-36">
-              <span class="star-item"></span>
+              <span class="star-item"
+              v-for="(item, index) in className2"
+              :key="index"
+              :class="className2"
+              ></span>
             </div>
             <span class="score">{{seller.foodScore}}</span>
           </div>
           <div class="delivery-wrapper">
             <span class="title">送达时间</span>
-            <span class="delivery">38分钟</span>
+            <span class="delivery">{{seller.deliveryTime}}分钟</span>
           </div>
         </div>
       </div>
@@ -32,7 +40,7 @@
       <div class="ratingselect">
         <div class="rating-type border-1px">
           <span class="block-positive active">全部
-            <span class="count">24</span>
+            <span class="count">{{seller.ratingCount}}</span>
           </span>
           <span class="block-positive">满意
             <span class="count">18</span>
@@ -48,24 +56,24 @@
       </div>
       <div class="rating-wrapper">
         <ul>
-          <li class="rating-item">
+          <li class="rating-item" v-for="(item, index ) in ratings" :key="index">
             <div class="avatar">
-              <img src="" alt="">
+              <img width="28" height="28" :src="ratings.avatar" alt="">
             </div>
             <div class="content">
-              <h1 class="name"></h1>
+              <h1 class="name">{{ratings.username}}</h1>
               <div class="star-wrapper">
                 <div class="star star-24">
                   <span class="star-item"></span>
                 </div>
-                <span class="delivery">30</span>
+                <span class="delivery">{{ratings.deliveryTime}}</span>
               </div>
-              <p class="text">不错粥挺好</p>
+              <p class="text">{{ratings.text}}</p>
               <div class="recommend">
                 <span class="icon-thumb_up"></span>
-                <span class="item">南瓜粥</span>
+                <span class="item" v-for="(item, index) in ratings.recommend" :key="index">{{item}}</span>
               </div>
-              <div class="time">2016-07-23</div>
+              <div class="time">{{ratings.rateTime}}</div>
             </div>
           </li>
         </ul>
@@ -75,6 +83,7 @@
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 export default {
   name: 'ratings',
   data () {
@@ -89,6 +98,8 @@ export default {
         console.log(res)
         if (res.data.errno === 0) {
           this.ratings = res.data.data
+          // html 渲染之后调用—initScroll方法
+          // this.score.push([this.ratings.score])
         }
       })
     this.$http.get('http://localhost:8080/static/seller.json')
@@ -97,19 +108,58 @@ export default {
         if (res.data.errno === 0) {
         // this.seller = res.data.data
           this.seller = Object.assign({}, this.seller, res.data.data)// 合并对象
+          // this.score.push(this.seller.serviceScore, this.seller.foodScore)
+          // console.log(this.score)
         }
       })
+    // html 渲染之后调用—initScroll方法
+    this.$nextTick(() => {
+      this._initscroll()
+      // this.className()
+    })
+  },
+  methods: {
+    _initscroll () {
+      this.ratingsScrall = new BScroll(this.$refs.ratingsContent, {
+        click: true
+      })
+    }
+    // starIndex () {
+    //   this.score = Math.floor(this.score.item)
+    //   console.log(this.score)
+    // }
+  },
+  computed: {
+    className () {
+      let name = []
+      let score = Math.floor(this.seller.serviceScore)
+      for (let i = 0; i < score; i++) {
+        name.push('on')
+      }
+      // while (name.length < 5) {
+      //   name.push('off')
+      // }
+      return name
+    },
+    className2 () {
+      let name = []
+      let score = Math.floor(this.seller.foodScore)
+      for (let i = 0; i < score; i++) {
+        name.push('on')
+      }
+      // while (name.length < 5) {
+      //   name.push("off")
+      // }
+      return name
+    }
   }
-  // computed: {
-  //   starIndex () {
-  //     for (let i = 0; i < 5; i++) {
-  //     }
-  //   }
-  // }
 }
-</script>
+</script>s
 
 <style lang="stylus" scoped>
+ul
+  li
+  list-style none;
 .ratings
   position absolute
   top 174px
@@ -117,17 +167,19 @@ export default {
   left 0
   width 100%
   overflow hidden
+  // .ratings-content
   .overview
     display flex
-    padding 0 18px
+    padding 18px 0
     .overiview-left
-      flex: 0 0 137px;
+      flex 0 0 137px
       padding 6px 0 6px 0
       width 137px
       border-right 1px solid rgba(7, 17, 27, 1)
       text-align center
       .scroe
         margin-bottom 6px
+        line-height 28px
         font-size 24px
         color #f90
       .title
@@ -151,10 +203,30 @@ export default {
           vertical-align top
           font-size 12px
           color #07111b
-          .star
+        .star
+          display inline-block
+          margin 0 12px
+          vertical-align top
+          font-size 0
+          .star-item
             display inline-block
-            margin 0 12px
-            vertical-align top
+            width 15px
+            height 15px
+            margin-right 6px
+            background-size 15px 15px
+            background-repeat: no-repeat
+          .star-item.on
+            background-image url('../../assets/start1.png')
+          .star-item.off
+            background-image url('../../assets/start2.png')
+          .star-item:last-child
+            margin-right 0
+        .score
+          display inline-block
+          line-height 18px
+          vertical-align top
+          font-size 12px
+          color #f90
       .delivery-wrapper
         font-size 0
         .title
@@ -168,5 +240,143 @@ export default {
   .split
     width 100%
     height 16px
-    border 1px solid rgba(7, 17, 21, 1)
+    border-top 1px solid rgba(7,17,27,.1)
+    border-bottom 1px solid rgba(7,17,27,.1)
+    background #f3f5f7
+    .rating-type
+      padding 18px 0
+      margin 0 18px
+      position relative
+      font-size 0
+      &::after
+        display block
+        position: absolute
+        left 0
+        bottom 0
+        width: 100%
+        border-top: 1px solid rgba(7,17,27,.1);
+        content " "
+      .block
+        display inline-block
+        padding 8px 12px
+        margin-right 8px
+        line-height 16px
+        border-radius 1px
+        font-size 12px
+        color #4d555d
+        .count
+          margin-left 2px
+          font-size 8px
+      .block.positive.active
+        background #00a0dc
+        color #fff
+      .block.positive
+        background rgba(0,160,220,.2)
+      .block.negative
+        background: rgba(77,85,93,.2)
+    .switch
+      padding 12px 18px
+      line-height: 24px
+      border-bottom 1px solid rgba(7,17,27,.1)
+      color: #93999f
+      font-size 0
+      .icon-check_circle
+        display inline-block
+        vertical-align top
+        margin-right 4px
+        font-size 24px
+      .text
+        display: inline-block
+        vertical-align: top
+        font-size: 12px
+    .switch.on
+      .icon-check_circle
+        color: #00c850
+  .rating-wrapper
+    padding 0 18px
+    .rating-item
+      display: flex
+      padding: 18px 0
+      position: relative
+      &:after
+        display: block
+        position: absolute
+        left: 0
+        bottom: 0
+        width: 100%
+        border-top: 1px solid rgba(7,17,27,.1)
+        content: " "
+      .avatar
+        -webkit-box-flex: 0
+        -ms-flex: 0 0 28px
+        flex: 0 0 28px
+        width: 28px
+        margin-right: 12px
+        img
+          border-radius 50%
+      .content
+        position: relative
+        -webkit-box-flex: 1
+        -ms-flex: 1
+        flex: 1
+        .name
+          margin-bottom: 4px
+          line-height: 12px
+          font-size: 10px
+          color: #07111b
+        .star-wrapper
+          margin-bottom: 6px
+          font-size: 0
+          vertical-align top
+          .star
+            display inline-block
+            margin 0 12px
+            vertical-align top
+            font-size 0
+            .star-item
+              display inline-block
+              width 15px
+              height 15px
+              margin-right 6px
+              background-size 15px 15px
+              background-repeat: no-repeat
+            .star-item.on
+              background-image url('../../assets/start1.png')
+          .delivery
+            display: inline-block
+            vertical-align: top
+            line-height: 12px
+            font-size: 10px
+            color: #93999f
+        .text
+          margin-bottom: 8px
+          line-height: 18px
+          color: #07111b
+          font-size: 12px
+        .recommend
+          line-height: 16px
+          font-size: 0
+          .icon-thumb_up
+            display: inline-block;
+            margin: 0 8px 4px 0;
+            font-size: 9px;
+            color: #00a0dc;
+            &:before
+              content: "\E901"
+          .item
+            display: inline-block;
+            margin: 0 8px 4px 0;
+            font-size: 9px;
+            padding: 0 6px;
+            border: 1px solid rgba(7,17,27,.1)
+            border-radius: 1px
+            color: #93999f
+            background: #fff
+        .time
+          position: absolute
+          top: 0
+          right: 0
+          line-height: 12px
+          font-size: 10px
+          color: #93999f
 </style>
--- yu-Teacher/ai/car_number/readme.md
+++ yu-Teacher/ai/car_number/readme.md
@@ -5,14 +5,3 @@
  - 海量数据， 构建好了训练模型
  给一张照片给模型， 分辨出来
  机器学习
- var ApiImaggClassifyClient = require('baidu-aip-sdk').imageClassify;// 图片分类
-var fs = require('fs'); // 文件模块
-
-var App_Id = "17798920";
-var ApI_KEY = "d1nztBqAVIYMW0V0OBSDNxVk";
-var SECRET_kEY = "DfMYXvEVi1Vx9wHk0825FngERPR4CxvA";
-// 内存 -> I/O
-// 图片加载后
-var iamge = fs.readFileSync('car.jpg').toString("base64");// 同步
-var client = new ApiImaggClassifyClient
-console.log(image);
