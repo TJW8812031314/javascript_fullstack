3bd9ce1771d6c4891082281423c0358dec4cb6c2
1362368453@qq.com <1362368453@qq.com>
2019-12-20 17:54:42 +0800

    手写webpack包

---
+++ webpack/readme.md
@@ -0,0 +1,28 @@
+# npm i webpack webpack-cli --save-dev
+# npm i lodash --save-dev
+# npm install style-loader css-loader --save-dev
+# npm init -y
+# npx webpack
+
+## dist 存放打包后的文件
+
+# src 存放入口文件
+
+# webpack.config.js // webpack的配置文件
+
+- webpack中的 常用模块(module) 用来如何处理项目中的不同类型模块
+
+- webpack 模块支持语法：
+  es6 import语法
+  Commonjs require() 语句
+  AMD define 和 require 语句
+  css/sass/less 文件中的@import 语句
+  样式 (url(...)) 或者html文件(<img src="...">)中的图片链接
+
+# npm i postcss-loader autoprefixer --save-dev 给css添加前缀
+
+autoprefixer 进行浏览器的部分兼容补全
+
+# 抽取样式将css单独打包
+  npm i mini-css-extract-plugin --save-dev
+
---
+++ webpack/webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',// 模式
+  output: {// 出口
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      // {
+      //   test: /\.css$/, //匹配文件
+      //   use: ['style-loader', 'css-loader'] // 指明使用什么加载器
+      // },
+      // {
+      //   test: /\.scss$/, //匹配文件
+      //   use: ['style-loader', 'css-loader', 'sass-loader'] // 指明使用什么加载器
+      // },
+      {
+        test: /\.(sc|c|sa)ss$/, //匹配文件
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {sourceMap: true}// 方便调试
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              sourceMap: true,
+              plugins: loader => [
+                require('autoprefixer')(),
+                //这里还能引入更多别的插件
+              ]
+          
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {sourceMap: true}//
+          }
+        ] 
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css', // 最终输出文件名
+      chunkFilename: '[id].css'
+    })
+  ]
+}
