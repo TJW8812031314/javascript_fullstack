56eb8d38ebcba9c6881fe64b9e0e512692c62b4f
1362368453@qq.com <1362368453@qq.com>
2019-12-23 21:43:59 +0800

    函数缓存和http缓存

---
+++ zhao-meng-Teacher/js/fp-memorize-http-cache/app.js
@@ -0,0 +1,45 @@
+var Koa = require('koa');
+var Router = require('koa-router');
+var koaStatic = require('koa-static')
+ 
+var app = new Koa();
+var router = new Router();
+ 
+// 把我们的文件路径 映射为 网络url
+// app1.js => localhost:9999/app1.js
+// app.use(koaStatic(__dirname))
+
+
+router.get('/', (ctx, next) => {
+  // ctx.router available
+  ctx.body = `
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <div id="app"></div>
+  <script src="./app1.js"></script>
+</body>
+</html>
+  `
+});
+ router.get('/app1.js', async(ctx) => {
+   console.log('app.js 请求')
+   const fs = require('fs')
+   const content = fs.readFileSync('./app1.js', 'utf8')
+   const time = Date.now() + 1000 * 30; // 30s
+   ctx.set('cache-control', 'public,max-age=30')
+   ctx.body = content
+ })
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+app.listen(9999, () => {
+  console.log('run http://localhost:9999')
+})
