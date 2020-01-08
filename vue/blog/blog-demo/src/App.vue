db1bd3bd89f0a5cdd6f6ad6f2fe0ac7053f4ef9d
1362368453@qq.com <1362368453@qq.com>
2020-01-08 10:01:04 +0800

    vue3.0 手写blog

--- vue/blog/blog-demo/src/App.vue
+++ vue/blog/blog-demo/src/App.vue
@@ -1,13 +1,14 @@
 <template>
   <div id="app">
     <h1>looke me</h1>
-    <BlogPost></BlogPost>
-    <router-view/>
+    <button @click="showHide">show/hide highlighted post</button>
+    <BlogPost v-for="blogPost in visibileBlogPosts" :post="blogPost" :key="blogPost.item"></BlogPost>
+    <!-- <router-view/> -->
   </div>
 </template>
 
 <script lang="ts">
- import BlogPost from "./components/BlogPost.vue";
+ import BlogPost, { Post } from "./components/BlogPost.vue";
  import { Component, Vue } from "vue-property-decorator";
 
   @Component({
@@ -16,7 +17,37 @@
     }
   })
   export default class App extends Vue {
+    private blogPost: Post[] = [
+      {
+        title: 'My first blogpost ever!',
+        body: 'welcome to my blog',
+        author: 'df',
+        datePosted: new Date(2020, 0, 7)
+      },
+      {
+        title: 'My second blogpost ever!',
+        body: 'welcome to my blog',
+        author: 'df',
+        datePosted: new Date(2020, 0, 8),
+        highlighted: true
+      },
+      {
+        title: 'My third blogpost ever!',
+        body: 'welcome to my blog',
+        author: 'df',
+        datePosted: new Date(2020, 0, 9)
+      }
+    ]
 
+    public showHighlighted: boolean = true
+
+    get visibileBlogPosts() {
+      return this.blogPost.filter((post: Post) => post.highlighted === undefined || post.highlighted === this.showHighlighted)
+    }
+
+    public showHide () {
+      this.showHighlighted = !this.visibileBlogPosts
+    }
   }
 </script>
 
