ed20fb1ca00338b42872bb7d283c064a2653d4d4
1362368453@qq.com <1362368453@qq.com>
2020-04-02 19:30:33 +0800

    webpack url-loader

--- webpack/webpack-day/index.js
+++ webpack/webpack-day/index.js
@@ -1,12 +1,17 @@
 import a from './a'
 import b from './b'
-import pic from './img/test'
+import pic from './img/test.jpg'
+
+var img = new Image()
+img.src = pic
+var root = document.getElementById('root')
+root.appendChild(img)
 
 // 既然wepack默认只认识js模块，那么遇到非js模块怎么办
 // loader
 a()
 b()
-
+console.log(pic)
 function lm() {
   console.log('welcome to lm')
 }
--- webpack/webpack-day/webpack.config.js
+++ webpack/webpack-day/webpack.config.js
@@ -7,5 +7,23 @@ module.exports = {
     // publicPath: "https://cdn.biadu.com",
     path: path.resolve(__dirname, "./build"),
     filename: "index.js"
+  },
+  module: {
+    // 遇到不认识的模块，就在这里找loader
+    rules: [
+       {
+         test: /\.jpg$/,
+         use: {
+           // url-loader 可以限制模块的体积，根据体积判断是否需要转换base64格式
+          loader: "url-loader",// 就是把模块，放在了另一个目录里，并且把位置
+          options: {
+            // name 打包前模块的名称，ext是打包前的模块格式
+            name: "[name]_[hash].[ext]" ,// test.jpg
+            outputPath: 'images/', // 输出路径
+            limit: 24000,// 图片大小临界值，当小于直接以base64格式
+          },
+         }
+       }
+    ]
   }
 }
